4d2029c04410364161154ff271f883db415f794b
Ayan Kumar <kumarayanatwork@gmail.com>
2026-02-01 13:35:04 +0530

    db: convex setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <p>Web</p>
+        <p>Widget</p>
         {JSON.stringify(users)}
       </div>
     </div>
