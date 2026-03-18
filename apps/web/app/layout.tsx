8e953cf90fc4f7eeb50c7952b6a6cdcc317556f2
Ayan Kumar <kumarayanatwork@gmail.com>
2026-03-18 13:52:09 +0530

    feat: initial commit

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@workspace/ui/lib/utils";
 
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+const outfit = Outfit({subsets:['latin'],variable:'--font-sans'});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,9 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", outfit.variable)}>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+        className={`${outfit.variable} ${fontMono.variable} font-sans antialiased `}
       >
         <ClerkProvider>
           <Providers>{children}</Providers>
