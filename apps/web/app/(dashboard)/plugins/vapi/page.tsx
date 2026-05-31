be0578c1a2a43d08db3407148f0e6b4b6d36056c
Ayan Kumar <kumarayanatwork@gmail.com>
2026-05-31 23:30:45 +0530

    building the vapiView component

--- apps/web/app/(dashboard)/plugins/vapi/page.tsx
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -1,5 +1,7 @@
+import { VapiView } from "@/app/modules/plugins/ui/views/vapi-view";
+
 const Page = () => {
-  return <div>Vapi</div>;
+  return <VapiView />;
 };
 
 export default Page;
---
+++ apps/web/app/modules/plugins/ui/views/vapi-view.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+  GlobeIcon,
+  PhoneCallIcon,
+  WorkflowIcon,
+  PhoneIcon,
+} from "lucide-react";
+import { Feature, PluginCard } from "../components/plugin-card";
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+
+const vapiFeatures: Feature[] = [
+  {
+    icon: GlobeIcon,
+    label: "Web voice calls",
+    description: "Voice chat directly in your app",
+  },
+  {
+    icon: PhoneIcon,
+    label: "Phone numbers",
+    description: "Get dedicated business lines",
+  },
+  {
+    icon: PhoneCallIcon,
+    label: "Outbound calls",
+    description: "Automated customer outreach",
+  },
+  {
+    icon: WorkflowIcon,
+    label: "Workflows",
+    description: "Custom conversation flows",
+  },
+];
+
+export const VapiView = () => {
+  const vapiPlugin = useQuery(api.private.plugins.getOne, { service: "vapi" });
+  return (
+    <div className="flex min-h-screen flex-col bg-muted p-8">
+      <div className="mx-auto w-full max-w-screen-md">
+        <div className="space-y-2">
+          <h1 className="text-2xl md:text-4xl">Vapi Plugin</h1>
+          <p className="text-muted-foreground">
+            Connect Vapi to enable AI voice calls and phone support
+          </p>
+        </div>
+        <div className="mt-8">
+          {vapiPlugin ? (
+            <p>Connected!</p>
+          ) : (
+            <PluginCard
+              serviceImage="/vapi.jpg"
+              serviceName="Vapi"
+              features={vapiFeatures}
+              isDisabled={vapiPlugin === undefined}
+              onSubmit={() => {}}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
