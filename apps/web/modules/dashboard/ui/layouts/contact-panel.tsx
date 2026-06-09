494cdc2ee1b89182517f236607a386d0a3d58ff5
Ayan Kumar <kumarayanatwork@gmail.com>
2026-06-09 13:13:40 +0530

    contact panel updates to show user-agent metadata info

--- apps/web/modules/dashboard/ui/layouts/contact-panel.tsx
+++ apps/web/modules/dashboard/ui/layouts/contact-panel.tsx
@@ -6,10 +6,10 @@ import { DicebearAvatar } from "@workspace/ui/components/dicebear-avatar";
 import { useQuery } from "convex/react";
 import { getCountryFlagUrl, getCountryFromTimezone } from "@/lib/country-utils";
 import { useParams } from "next/navigation";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import { Button } from "@workspace/ui/components/button";
 import Link from "next/link";
-import { MailIcon } from "lucide-react";
+import { ClockIcon, GlobeIcon, MailIcon, MonitorIcon } from "lucide-react";
 import {
   Accordion,
   AccordionContent,
@@ -18,6 +18,19 @@ import {
 } from "@workspace/ui/components/accordion";
 import Bowser from "bowser";
 import { userAgent } from "next/server";
+
+type InfoItem = {
+  label: string;
+  value: string | React.ReactNode;
+  className?: string;
+};
+
+type InfoSection = {
+  id: string;
+  icon: React.ComponentType<{ className?: string }>;
+  title: string;
+  items: InfoItem[];
+};
 
 export const ContactPanel = () => {
   const params = useParams();
@@ -62,6 +75,96 @@ export const ContactPanel = () => {
     return getCountryFromTimezone(contactSession?.metadata?.timezone);
   }, [contactSession?.metadata?.timezone]);
 
+  const accordionSections = useMemo<InfoSection[]>(() => {
+    if (!contactSession?.metadata) {
+      return [];
+    }
+
+    return [
+      {
+        id: "device-info",
+        icon: MonitorIcon,
+        title: "Device Information",
+        items: [
+          {
+            label: "Browser",
+            value:
+              userAgentInfo.browser +
+              (userAgentInfo.browserVersion
+                ? ` ${userAgentInfo.browserVersion}`
+                : ""),
+          },
+          {
+            label: "OS",
+            value:
+              userAgentInfo.os +
+              (userAgentInfo.osVersion ? ` ${userAgentInfo.osVersion}` : ""),
+          },
+          {
+            label: "Device",
+            value:
+              userAgentInfo.device + userAgentInfo.deviceModel
+                ? ` ${userAgentInfo.deviceModel}`
+                : "",
+            className: "capitalize",
+          },
+          {
+            label: "Screen",
+            value: contactSession.metadata.screenResolution,
+          },
+          {
+            label: "Viewport",
+            value: contactSession.metadata.viewportSize,
+          },
+          {
+            label: "Cookies",
+            value: contactSession.metadata.cookieEnabled
+              ? "Enabled"
+              : "Disabled",
+          },
+        ],
+      },
+      {
+        id: "location-info",
+        icon: GlobeIcon,
+        title: "Location & Language",
+        items: [
+          ...(countryInfo
+            ? [
+                {
+                  label: "Country",
+                  value: <span>{countryInfo.name}</span>,
+                },
+              ]
+            : []),
+          {
+            label: "Language",
+            value: contactSession.metadata.language,
+          },
+          {
+            label: "Timezone",
+            value: contactSession.metadata.timezone,
+          },
+          // {
+          //   label: "UTC Offset",
+          //   value: `${-contactSession.metadata.timezoneOffset / 60} hours`,
+          // },
+        ],
+      },
+      {
+        id: "session-details",
+        title: "Session Details",
+        icon: ClockIcon,
+        items: [
+          {
+            label: "Session Started",
+            value: new Date(contactSession._creationTime).toLocaleString(),
+          },
+        ],
+      },
+    ];
+  }, [contactSession, userAgentInfo, countryInfo]);
+
   if (contactSession === undefined || contactSession == null) {
     return null;
   }
@@ -94,6 +197,45 @@ export const ContactPanel = () => {
             <span>Send Email</span>
           </Link>
         </Button>
+      </div>
+      <div>
+        {contactSession.metadata && (
+          <Accordion className="w-full" collapsible type="single">
+            {accordionSections.map((section) => (
+              <AccordionItem
+                key={section.id}
+                value={section.id}
+                className="rounded-none outline-none has-focus-visible:z-10 has-focus-visible:border-ring has-focus-visible:ring-[3px] 
+                has-focus-visible:ring-ring/50"
+              >
+                <AccordionTrigger
+                  className="flex w-full flex-1 items-start justify-between gap-4 rounded-none bg-accent px-5 py-4 text-left 
+                font-medium text-sm outline-none transition-all hover:no-underline disabled:pointer-events-none disabled:opacity-50"
+                >
+                  <div className="flex items-center gap-4">
+                    <section.icon className="size-4 shrink-0" />
+                    <span>{section.title}</span>
+                  </div>
+                </AccordionTrigger>
+                <AccordionContent className="px-5 py-4">
+                  <div className="space-y-2 text-sm">
+                    {section.items.map((item) => (
+                      <div
+                        className="flex justify-between"
+                        key={`${section.id}-${item.label}`}
+                      >
+                        <span className="text-muted-foreground">
+                          {item.label}
+                        </span>
+                        <span className={item.className}>{item.value}</span>
+                      </div>
+                    ))}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        )}
       </div>
     </div>
   );
